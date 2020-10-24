1dd2ca0bc85c5fa987841019bcf71cc5fae2c39d
marc <marcbosch90@gmail.com>
2020-10-25 01:08:46 +0200

    Falta acabar classes MultiLine, Console i StringLine. Entendre com canviar els metodes de Observer/Observable per els de PropertyChangeListener

--- practica1/mvc/MultiLine.java
+++ practica1/mvc/MultiLine.java
@@ -1,6 +1,9 @@
 package mvc;
 
 import java.util.Observable;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 
 public class MultiLine extends Observable {
 
@@ -17,6 +20,21 @@ public class MultiLine extends Observable {
         this.MAX_Lines = maxfils;
         this.lines = new Line[MAX_Lines];
         this.lines[this.posy] = new Line(this.MAX_Cols, this.MAX_Lines);
+    }
+
+    public Boolean newLine() {
+        //comprovem la possibilitat de que no estiguem a la última línia,
+        //pero que no puguem suportar més Lines (a partir de la última actual)
+        if (this.lines[this.MAX_Lines - 1] == null) {
+            this.posy++;
+            for(int i = this.MAX_Lines -1; i> this.posy; i--){
+                this.lines[i] = this.lines[i-1];
+            }
+            this.lines[this.posy] = new Line(this.MAX_Cols, this.MAX_Lines);
+            this.lines[this.posy].setMode(this.mode);
+            this.setChanged(); //hem d'utilitzar metodes de PropertyChangeListener
+            this.
+        }
     }
 
 }
